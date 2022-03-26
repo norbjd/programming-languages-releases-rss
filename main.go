cd17309ccbf45171c27a96e1c9779de1a72528cd
norbjd <norbjd@users.noreply.github.com>
2022-03-26 16:00:03 +0100

    Add feed update date + convert time to UTC

--- main.go
+++ main.go
@@ -122,6 +122,7 @@ func createFeedFromGithubGraphQLAPI(ctx context.Context, client *githubv4.Client
 		Title:       fmt.Sprintf("%s releases", repo.Language),
 		Link:        &feeds.Link{Href: fmt.Sprintf("%s/tags", q.Repository.URL)},
 		Description: fmt.Sprintf("%s releases", repo.Language),
+		Updated:     time.Now().UTC(),
 	}
 
 	edges := q.Repository.Ref.Edges
@@ -133,7 +134,7 @@ func createFeedFromGithubGraphQLAPI(ctx context.Context, client *githubv4.Client
 		item := &feeds.Item{
 			Title:   tagName,
 			Link:    &feeds.Link{Href: fmt.Sprintf("%s/releases/tag/%s", q.Repository.URL, tagName)},
-			Created: edge.Node.Target.Tag.Tagger.Date,
+			Created: edge.Node.Target.Tag.Tagger.Date.UTC(),
 		}
 
 		feed.Items = append(feed.Items, item)
